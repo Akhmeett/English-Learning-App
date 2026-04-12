2d844c9828e0172e0d396d15179b16bb6d5be8c4
Akhmeett <mustapaahmet2007@gmail.com>
2026-04-12 15:37:29 +0500

    login, vocabulary, layout pages added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Profile } from './profile/profile';
+import { Quiz } from './quiz/quiz';
+import { Vocabulary } from './vocabulary/vocabulary';
+import { Layout } from './layout/layout';
+export const routes: Routes = [
+    {path: '', component: Login},
 
-export const routes: Routes = [];
+    {path: 'app', component:Layout,
+        children: [
+            {path:'home', component: Home},
+            {path:'profile', component:Profile},
+            {path:'quiz', component: Quiz},
+            {path:'vocabulary',component:Vocabulary}
+        ]
+    }
+];
---
+++ frontend/src/app/vocabulary/vocabulary.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vocabulary',
+  imports: [],
+  templateUrl: './vocabulary.html',
+  styleUrl: './vocabulary.css',
+})
+export class Vocabulary {}
