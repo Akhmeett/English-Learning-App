c9c8aeeb78c3cb37e54e4d8101af16434fe9295a
Akhmeett <mustapaahmet2007@gmail.com>
2026-04-20 13:16:10 +0500

    backend added

--- frontend/src/app/vocabulary/vocabulary.ts
+++ frontend/src/app/vocabulary/vocabulary.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-vocabulary',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './vocabulary.html',
   styleUrl: './vocabulary.css',
 })
-export class Vocabulary {}
+export class Vocabulary {
+  isModalOpen = false;
+  selectedLevel = '';
+  newWord = {
+    english: '',
+    kazakh: '',
+    example: '',
+    level: ''
+  };
+  openModal() {
+    this.isModalOpen = true;
+  }
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedLevel = '';
+    this.newWord = { english: '', kazakh: '', example: '', level: '' };
+  }
+  selectLevel(level: string) {
+    this.selectedLevel = level;
+    this.newWord.level = level;
+  }
+  addWord() {
+    if (!this.newWord.english || !this.newWord.kazakh || !this.selectedLevel) return;
+    console.log('Жаңа сөз:', this.newWord);
+    this.closeModal();
+  }
+}
